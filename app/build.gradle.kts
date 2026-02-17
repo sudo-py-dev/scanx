9b54a0bb59b4fde7ce4c8a9bfe8533dca90a5c6c
sudo-py-dev <sudopydev@gmail.com>
2026-02-17 17:45:11 +0200

    feat: Add privacy policy, update app version to 1.1, and remove the AD_ID permission.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.scanx.qrscanner"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
 
